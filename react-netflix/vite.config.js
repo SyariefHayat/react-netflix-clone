e3ef0b661b3d8542d2874439903bbd7c1deeaee0
SyariefHayat <muhammadsyarifhayat@gmail.com>
2024-09-13 13:13:54 +0700

    update: vite config

--- react-netflix/vite.config.js
+++ react-netflix/vite.config.js
@@ -5,6 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
+  build: {
+    outDir: "dist",
+  },
   plugins: [react()],
   resolve: {
     alias: {
