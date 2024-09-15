8847834f38c50a567dff6d0781e17e8a21435d2a
SyariefHayat <muhammadsyarifhayat@gmail.com>
2024-09-15 23:26:30 +0700

    fix: cors

--- api-netflix/index.js
+++ api-netflix/index.js
@@ -17,7 +17,10 @@ const PORT = API_PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://react-netflix-clone-sand.vercel.app',
+  origin: 'https://react-netflix-clone-sand.vercel.app', 
+  methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials: true, 
 }));
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
